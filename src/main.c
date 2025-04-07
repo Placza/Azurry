f7522ddeebc0b2428d78a54335c66bbb8e4db4cf
Emanuel Rod <emiroker321@gmail.com>
2025-04-07 23:24:22 +0200

    Added the main_menu.ui file and updated the main.c file to match the main_menu.ui file; added buttons above canvas

--- src/main.c
+++ src/main.c
@@ -8,24 +8,23 @@ static void app_activate (GApplication *app, gpointer *user_data) {
 	* <---parent                      child--->
 	*/
 
+	GtkBuilder *build;
 	GtkWidget *win;
 	GtkWidget *drawing_area;
 	GtkGesture *drag;
 
-	//creates a GTK window
-	win = gtk_application_window_new (GTK_APPLICATION (app));
-	gtk_window_set_title (GTK_WINDOW (win), "Azurry");
-	gtk_window_set_default_size (GTK_WINDOW (win), 1000, 600);
+	build = gtk_builder_new_from_file ("../data/ui/main_menu.ui");
 
+	win = GTK_WIDGET (gtk_builder_get_object (build, "win"));
+	gtk_window_set_application (GTK_WINDOW (win), GTK_APPLICATION (app));
+	
 	//creates a drawing area
-	drawing_area = gtk_drawing_area_new ();
-	gtk_widget_set_size_request (drawing_area, 100, 100);
+	drawing_area = GTK_WIDGET (gtk_builder_get_object (build, "drawing_area"));
 	gtk_drawing_area_set_draw_func (GTK_DRAWING_AREA (drawing_area),
 									draw_callback, NULL, NULL);
 	g_signal_connect_after (drawing_area, "resize", G_CALLBACK (resize_callback), NULL);
-	gtk_window_set_child (GTK_WINDOW (win), drawing_area);
 
-
+	
 	//initializes the drag object
 	drag = gtk_gesture_drag_new ();
 	gtk_gesture_single_set_button (GTK_GESTURE_SINGLE (drag), GDK_BUTTON_PRIMARY);

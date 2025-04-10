a76ec679c8e1ab12384add2ee86d45d7bfec720f
Emanuel Rod <emiroker321@gmail.com>
2025-04-10 10:51:54 +0200

    Updated the canvas and toold .c file

--- src/azurry_canvas.c
+++ src/azurry_canvas.c
@@ -1,17 +1,22 @@
 #include <gtk/gtk.h>
 #include "../include/azurry_canvas.h"
 
+/*initializes the canvas*/
 Azurry_canvas* azurry_canvas_create (GtkWidget *drawing_area) {
     Azurry_canvas *canvas = (Azurry_canvas*) g_malloc (sizeof (Azurry_canvas));
     canvas->drawing_area = drawing_area;
     gtk_drawing_area_set_draw_func (GTK_DRAWING_AREA (drawing_area), 
-                                    azurry_canvas_draw_callback, canvas, NULL);
-    g_signal_connect_after (drawing_area, "resize", G_CALLBACK (azurry_canvas_resize_callback), canvas);
+                                    azurry_canvas_draw_callback, canvas, NULL); //link a draw callback function
+    g_signal_connect_after (drawing_area, "resize", G_CALLBACK (azurry_canvas_resize_callback), canvas); //link a resize callback function
     canvas->surface = NULL;
     canvas->current_tool = 0;
     return canvas;
 }
 
+/*
+  function that is being called after the GtkDrawingArea gets initialized for the canvas class
+  used to fill in the surface
+*/
 void azurry_canvas_draw_callback (GtkDrawingArea *area, cairo_t *cairo, int width, int height, gpointer data) {
     Azurry_canvas *canvas = (Azurry_canvas*) data;
     g_print("Drawing with current tool: %d\n", canvas->current_tool);
@@ -20,13 +25,16 @@ void azurry_canvas_draw_callback (GtkDrawingArea *area, cairo_t *cairo, int widt
 	cairo_paint (cairo);
 }
 
+/*resize handling function that creates a new surface if one isn't created and destroys one if it already exists*/
 void azurry_canvas_resize_callback (GtkWidget *widget, int width, int height, gpointer data) {
-    Azurry_canvas *canvas = (Azurry_canvas*) data; 
+    Azurry_canvas *canvas = (Azurry_canvas*) data; //link the canvas surface
 
+    //if a surface exists, destroy it
 	if (canvas->surface) {
 		cairo_surface_destroy (canvas->surface);
 		canvas->surface = NULL;
 	}
+    //if a sruface doesn't exist, create a new one
 	if (gtk_native_get_surface (gtk_widget_get_native (widget))) {
 		canvas->surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32,
 											  gtk_widget_get_width (widget), 
@@ -34,7 +42,8 @@ void azurry_canvas_resize_callback (GtkWidget *widget, int width, int height, gp
 		
 		cairo_t *cairo;
 		cairo = cairo_create (canvas->surface);
-
+        
+        //paints the surface white
 		cairo_set_source_rgb (cairo, 255, 255, 255);
 		cairo_paint (cairo);
 		cairo_destroy (cairo); 
--- src/azurry_tools.c
+++ src/azurry_tools.c
@@ -1,12 +1,14 @@
 #include <gtk/gtk.h>
 #include "../include/azurry_tools.h"
 
+/*constructor for the tool class*/
 Azurry_tool* azurry_tool_create () {
     Azurry_tool *tool = (Azurry_tool*) malloc (sizeof (Azurry_tool));
     tool->appy = NULL;
     return tool;
 }
 
+/*constructor for the pointer tool class*/
 Azurry_pointer_tool *azurry_pointer_tool_create (Azurry_tool *parent, int size) {
     Azurry_pointer_tool *pointer_tool = (Azurry_pointer_tool*) malloc (sizeof (Azurry_pointer_tool));
     pointer_tool->parent = parent;
@@ -14,15 +16,24 @@ Azurry_pointer_tool *azurry_pointer_tool_create (Azurry_tool *parent, int size) 
     return pointer_tool;
 }
 
+/*
+  initializes the pointer tool's parent's function to its usage function
+  used for when we want to switch between tools
+*/
 void azurry_pointer_tool_use (Azurry_pointer_tool *self) {
     self->parent->appy = azurry_pointer_tool_apply;
 }
 
+/*pointer tool's usage function - what to perfomr when active*/
 void azurry_pointer_tool_apply (void *self, cairo_surface_t *cairo, int x, int y) {
     Azurry_pointer_tool *pointer_tool = (Azurry_pointer_tool*) self;
     g_print ("Using pointer %d\n", pointer_tool->size);
 }
 
+/*
+  function to call when we want to use a tool
+  after we pick a tool, we can apply it directly ba calling this function
+*/
 void azurry_tool_use (Azurry_tool *self, void *tool, cairo_surface_t *cairo, int x, int y) {
     self->appy(tool, cairo, x, y);
 }
